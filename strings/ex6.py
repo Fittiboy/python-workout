984d9b3cde697784b8055e6e5ed0ad6db75c6c8b
Fitti <dev@fitti.io>
2022-05-29 12:28:46 +0200

    ex6 Beyond the exercise 1

--- strings/ex6.py
+++ strings/ex6.py
@@ -5,5 +5,17 @@ def pl_sentence(sentence):
     return " ".join(map(pig_latin, sentence.split()))
 
 
+# Beyond the exercise
+def nonsense():
+    sentence = []
+    with open("./nonsense.txt") as ns_file:
+        lines = ns_file.read().split('\n')
+    for i, line in enumerate(lines[:10]):
+        sentence.append(line.split()[i])
+    return " ".join(sentence)
+
+
 if __name__ == "__main__":
     print(pl_sentence("this is a test translation"))
+    print("\nBeyond the exercise\n")
+    print(nonsense())
